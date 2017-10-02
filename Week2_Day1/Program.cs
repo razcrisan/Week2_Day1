17d01144a589926a02538d149197a7cead103076
razcrisan <rcrisan13@gmail.com>
2017-10-02 10:51:39 -0400

    second commit

--- Week2_Day1/Program.cs
+++ Week2_Day1/Program.cs
@@ -10,6 +10,25 @@ namespace Week2_Day1
     {
         static void Main(string[] args)
         {
+
+            int[] favNumbers = { 61, 22, 3, 44, 105, 36 };
+            //  0   1   2   3    4   5 etc. is how you pull elements, starting with index "0"
+            // this initialized the array so we can start storing stuff in it
+            // useful because you dont have to make 6 different "int" variables
+
+            string[] studentNames = { "Raz", "Monica", "David", "Brian", "Ali", };
+            // this is how you initialize an array of STRINGS
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
